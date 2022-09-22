df0ca02533295978913bac87f78f813b8f6d4143
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-22 08:28:51 -0500

    wip: add tracer service

--- src/services/tracer.service.ts
+++ src/services/tracer.service.ts
@@ -1,3 +1,57 @@
-export function tracerService() {
-  return {};
+import type { Tracer } from '@aws-lambda-powertools/tracer';
+import type { Segment, Subsegment } from 'aws-xray-sdk-core';
+import {
+  onErrorAsyncHookHandler,
+  onRequestAsyncHookHandler,
+  onResponseAsyncHookHandler,
+} from 'fastify';
+import type { TracerOptions } from '../types';
+
+export function tracerService(target: Tracer, options: TracerOptions) {
+  let lambdaSegment: Subsegment | Segment;
+
+  const open = (): void => {
+    lambdaSegment = target.getSegment();
+    const handlerSegment = lambdaSegment.addNewSubsegment(
+      `## ${process.env._HANDLER}`
+    );
+    target.setSegment(handlerSegment);
+  };
+
+  const close = (): void => {
+    const subsegment = target.getSegment();
+    subsegment.close();
+    target.setSegment(lambdaSegment as Segment);
+  };
+
+  const onRequestHook: onRequestAsyncHookHandler = async () => {
+    if (target.isTracingEnabled()) {
+      open();
+      target.annotateColdStart();
+      target.addServiceNameAnnotation();
+    }
+  };
+
+  const onResponseHook: onResponseAsyncHookHandler = async (request, reply) => {
+    if (target.isTracingEnabled()) {
+      if (options?.captureResponse ?? true) {
+        target.addResponseAsMetadata(reply.raw, process.env._HANDLER);
+      }
+
+      close();
+    }
+  };
+
+  const onErrorHook: onErrorAsyncHookHandler = async (error) => {
+    if (target.isTracingEnabled()) {
+      target.addErrorAsMetadata(error as unknown as Error);
+      close();
+    }
+  };
+
+  return {
+    onRequest: onRequestHook,
+    onResponse: onResponseHook,
+    onError: onErrorHook,
+  };
 }
