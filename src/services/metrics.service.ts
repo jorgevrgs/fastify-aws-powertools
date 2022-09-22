27b6808b4159eeac073392c8c99785a4fe97c4e3
Jorge Vargas <jorge.vargas@zemoga.com>
2022-09-22 08:23:04 -0500

    wip: add metrics service

--- src/services/metrics.service.ts
+++ src/services/metrics.service.ts
@@ -1,3 +1,52 @@
-export function metricsService() {
-  return {};
+import type { Metrics } from '@aws-lambda-powertools/metrics';
+import {
+  onErrorAsyncHookHandler,
+  onRequestAsyncHookHandler,
+  onResponseAsyncHookHandler,
+} from 'fastify';
+import { MetricsOptions } from '../types';
+
+export function metricsService(
+  target: Metrics | Metrics[],
+  options: MetricsOptions = {}
+) {
+  const metricsInstances = target instanceof Array ? target : [target];
+
+  const onRequestHook: onRequestAsyncHookHandler = async (request) => {
+    metricsInstances.forEach((metrics: Metrics) => {
+      metrics.setFunctionName(request.awsLambda.context.functionName);
+      const { throwOnEmptyMetrics, defaultDimensions, captureColdStartMetric } =
+        options;
+
+      if (throwOnEmptyMetrics !== undefined) {
+        metrics.throwOnEmptyMetrics();
+      }
+
+      if (defaultDimensions !== undefined) {
+        metrics.setDefaultDimensions(defaultDimensions);
+      }
+
+      if (captureColdStartMetric !== undefined) {
+        metrics.captureColdStartMetric();
+      }
+    });
+  };
+
+  const onResponseHook: onResponseAsyncHookHandler = async () => {
+    metricsInstances.forEach((metrics: Metrics) => {
+      metrics.publishStoredMetrics();
+    });
+  };
+
+  const onErrorHook: onErrorAsyncHookHandler = async () => {
+    metricsInstances.forEach((metrics: Metrics) => {
+      metrics.publishStoredMetrics();
+    });
+  };
+
+  return {
+    onRequest: onRequestHook,
+    onResponse: onResponseHook,
+    onError: onErrorHook,
+  };
 }
