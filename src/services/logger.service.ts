b9b455e16478c1d4a9455ba39c237bac4035d620
Jorge Vargas <jorgevargasla@gmail.com>
2024-09-23 20:58:21 -0500

    feat(logger): add setup clean up function

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,10 +1,12 @@
 import type { Logger } from '@aws-lambda-powertools/logger';
 import type { InjectLambdaContextOptions } from '@aws-lambda-powertools/logger/lib/cjs/types/Logger';
 import type {
+  FastifyRequest,
   onErrorAsyncHookHandler,
   onRequestAsyncHookHandler,
   onResponseAsyncHookHandler,
 } from 'fastify';
+import { LOGGER_KEY, POWERTOOLS_REQUEST_KEY } from '../constants';
 import { isAwsLambdaRequest } from '../helpers';
 
 export function loggerService(
@@ -17,6 +19,13 @@ export function loggerService(
 
   const loggers = Array.isArray(target) ? target : [target];
 
+  const setCleanupFunction = (request: FastifyRequest) => {
+    request[POWERTOOLS_REQUEST_KEY] = {
+      ...request[POWERTOOLS_REQUEST_KEY],
+      [LOGGER_KEY]: onResponseOrErrorHandler,
+    };
+  };
+
   const onRequestHook: onRequestAsyncHookHandler = async (request, _reply) => {
     if (!isAwsLambdaRequest(request)) {
       request.log.warn('Request does not contain AWS Lambda object');
@@ -24,6 +33,10 @@ export function loggerService(
     }
 
     for (const logger of loggers) {
+      if (isResetStateEnabled) {
+        setCleanupFunction(request);
+      }
+
       logger.addContext(request.awsLambda.context);
     }
   };
